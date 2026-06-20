5332feba7e52f5b2381cf2d8046d664acaceaa25
Prashant <prashantkarn@Prashants-MacBook-Air.local>
2026-06-20 12:06:26 +0545

    add paragraph

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
     <title>Home</title>
 </head>
 <body>
-    
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias laudantium cum itaque. Natus, laudantium accusamus harum eum corporis odio. Ipsa perferendis quidem, recusandae repellendus dolore ullam excepturi voluptatem reiciendis totam accusantium sequi nam pariatur.</p>
 </body>
 </html>
